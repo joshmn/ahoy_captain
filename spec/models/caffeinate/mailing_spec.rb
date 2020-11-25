5b38a8b490016d0ef2afdda426276eb4d72b9a0a
Josh Brody <josh@josh.mn>
2020-11-25 12:57:36 -0600

    mailing spec

---
+++ spec/models/caffeinate/mailing_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe ::Caffeinate::Mailing do
+
+  let!(:campaign) { create(:caffeinate_campaign, slug: :caffeinate_active_record_extension) }
+  let(:subscription) { create(:caffeinate_campaign_subscription, caffeinate_campaign: campaign) }
+  let!(:unsent_mailings) { create_list(:caffeinate_mailing, 5, :unsent, caffeinate_campaign_subscription: subscription) }
+  let!(:sent_mailings) { create_list(:caffeinate_mailing, 3, :sent, caffeinate_campaign_subscription: subscription) }
+  let!(:skipped_mailings) { create_list(:caffeinate_mailing, 2, :skipped, caffeinate_campaign_subscription: subscription) }
+
+  context '#unsent' do
+    it 'has 5 unsent mailings' do
+      expect(described_class.unsent.count).to eq(5)
+      expect(described_class.unsent).to eq(unsent_mailings)
+    end
+  end
+
+  context '#sent' do
+    it 'has 3 sent mailings' do
+      expect(described_class.sent.count).to eq(3)
+      expect(described_class.sent).to eq(sent_mailings)
+    end
+  end
+
+  context '#skipped' do
+    it 'has 3 skipped mailings' do
+      expect(described_class.skipped.count).to eq(2)
+      expect(described_class.skipped).to eq(skipped_mailings)
+      expect(described_class.unskipped.count).to eq(8)
+    end
+  end
+end
