68ced6f2ec7c8b7bd8b466018b4d56aa98e99650
Josh Brody <josh@josh.mn>
2020-11-27 17:04:41 -0600

    dependent: :destroy

--- lib/caffeinate/active_record/extension.rb
+++ lib/caffeinate/active_record/extension.rb
@@ -5,7 +5,7 @@ module Caffeinate
     module Extension
       # Adds the associations for a subscriber
       def caffeinate_subscriber
-        has_many :caffeinate_campaign_subscriptions, as: :subscriber, class_name: '::Caffeinate::CampaignSubscription'
+        has_many :caffeinate_campaign_subscriptions, as: :subscriber, class_name: '::Caffeinate::CampaignSubscription', dependent: :destroy
         has_many :caffeinate_campaigns, through: :caffeinate_campaign_subscriptions, class_name: '::Caffeinate::Campaign'
         has_many :caffeinate_mailings, through: :caffeinate_campaign_subscriptions, class_name: '::Caffeinate::Mailing'
 
