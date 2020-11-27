ab2702eb72ae1ba08d2a198095f9aa87c9063922
Josh Brody <josh@josh.mn>
2020-11-26 22:05:02 -0600

    update

--- lib/caffeinate/engine.rb
+++ lib/caffeinate/engine.rb
@@ -8,18 +8,16 @@ module Caffeinate
   class Engine < ::Rails::Engine
     isolate_namespace Caffeinate
 
+    config.to_prepare do
+      Dir.glob(Rails.root.join("app/drippers/**/*.rb")).each do |file|
+        load file
+      end
+    end
+
     ActiveSupport.on_load(:action_mailer) do
       include ::Caffeinate::ActionMailer::Extension
       ::ActionMailer::Base.register_interceptor(::Caffeinate::ActionMailer::Interceptor)
       ::ActionMailer::Base.register_observer(::Caffeinate::ActionMailer::Observer)
-    end
-
-    unless config.eager_load
-      config.to_prepare do
-        Dir.glob("#{Rails.root}/app/drippers/**/*.rb").each do |f|
-          require f
-        end
-      end
     end
 
     ActiveSupport.on_load(:active_record) do
