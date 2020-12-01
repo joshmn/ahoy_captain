c1e4d635fd25f36e72c000f10d93b7d728aff516
Josh Brody <josh@josh.mn>
2020-12-01 10:27:59 -0600

    batching

--- lib/caffeinate/dripper/batching.rb
+++ lib/caffeinate/dripper/batching.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 module Caffeinate
+  module Dripper
   # Includes batch support for setting the batch size for Perform
-  module Batching
-    def self.included(klass)
-      klass.extend ClassMethods
-    end
-
-    module ClassMethods
-      def batch_size(num)
-        @_batch_size = num
+    module Batching
+      def self.included(klass)
+        klass.extend ClassMethods
       end
 
-      def _batch_size
-        @_batch_size || ::Caffeinate.config.batch_size
+      module ClassMethods
+        def batch_size=(num)
+          @batch_size = num
+        end
+
+        def batch_size
+          @batch_size || ::Caffeinate.config.batch_size
+        end
       end
     end
   end
---
+++ spec/caffeinate/dripper/cases/batching_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ::Caffeinate::Dripper::Batching do
+  class BatchingDripper < ::Caffeinate::Dripper::Base; end
+
+  context '.batch_size' do
+    it 'equals config.batch_size by default' do
+      expect(BatchingDripper.batch_size).to eq(::Caffeinate.config.batch_size)
+    end
+  end
+
+  context '.batch_size=' do
+    it 'sets the batch_size' do
+      original = BatchingDripper.batch_size
+      expect {
+        BatchingDripper.batch_size = 5
+      }.to change(BatchingDripper, :batch_size).to(5)
+      BatchingDripper.batch_size = original
+    end
+  end
+end
