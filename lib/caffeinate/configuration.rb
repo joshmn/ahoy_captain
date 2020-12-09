d43cd2b9bb31324e91a3b30b2144bd0e03ea364c
Josh Brody <josh@josh.mn>
2020-12-08 18:18:34 -0600

    Add the implicit_campaign option to the config

--- lib/caffeinate/configuration.rb
+++ lib/caffeinate/configuration.rb
@@ -3,7 +3,7 @@
 module Caffeinate
   # Global configuration
   class Configuration
-    attr_accessor :now, :async_delivery, :mailing_job, :batch_size, :drippers_path
+    attr_accessor :now, :async_delivery, :mailing_job, :batch_size, :drippers_path, :implicit_campaigns
 
     def initialize
       @now = -> { Time.current }
@@ -11,12 +11,18 @@ module Caffeinate
       @mailing_job = nil
       @batch_size = 1_000
       @drippers_path = "app/drippers"
+      @implicit_campaigns = true
     end
 
     def now=(val)
       raise ArgumentError, '#now must be a proc' unless val.respond_to?(:call)
 
       super
+    end
+
+    # Automatically create a `::Caffeinate::Campaign` object if not found per `Dripper.inferred_campaign_slug`
+    def implicit_campaigns?
+      @implicit_campaigns == true
     end
 
     # The current time, for database calls
