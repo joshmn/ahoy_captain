8eca22a0dca50a6682b7855c2de9b22af0a34b65
Josh Brody <josh@josh.mn>
2021-01-18 12:14:09 -0600

    remove unnecessary comment

--- lib/caffeinate/action_mailer/interceptor.rb
+++ lib/caffeinate/action_mailer/interceptor.rb
@@ -5,7 +5,6 @@ module Caffeinate
     # Handles the evaluation of a drip against a mailing to determine if it ultimately gets delivered.
     # Also invokes the `before_send` callbacks.
     class Interceptor
-      # Handles `before_send` callbacks for a `Caffeinate::Dripper`
       def self.delivering_email(message)
         mailing = message.caffeinate_mailing
         return unless mailing
