f74158e91e99bedd40f9d2d4010d28684ea15c46
Ariel Fogel <fogeltine@gmail.com>
2023-07-29 15:05:47 +0300

    correct the import map load order

--- app/helpers/ahoy_captain/application_helper.rb
+++ app/helpers/ahoy_captain/application_helper.rb
@@ -2,11 +2,11 @@ module AhoyCaptain
   module ApplicationHelper
     def ahoy_captain_importmap_tags(entry_point = "application", shim: true)
       safe_join [
-        javascript_inline_importmap_tag(AhoyCaptain.importmap.to_json(resolver: self)),
-        javascript_importmap_module_preload_tags(AhoyCaptain.importmap),
-        javascript_import_module_tag(entry_point),
-        (javascript_importmap_shim_nonce_configuration_tag if shim),
         (javascript_importmap_shim_tag if shim),
+        (javascript_importmap_shim_nonce_configuration_tag if shim),
+        javascript_import_module_tag(entry_point),
+        javascript_importmap_module_preload_tags(AhoyCaptain.importmap),
+        javascript_inline_importmap_tag(AhoyCaptain.importmap.to_json(resolver: self)),
       ].compact, "\n"
     end
 
