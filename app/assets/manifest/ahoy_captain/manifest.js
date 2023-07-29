34b6d169048c45b4d21505ea84ce90b7996fba30
Josh Brody <git@josh.mn>
2023-07-29 09:23:19 -0500

    add favicons

--- app/assets/manifest/ahoy_captain/manifest.js
+++ app/assets/manifest/ahoy_captain/manifest.js
@@ -1,1 +1,2 @@
 //= link_tree ../../javascript/ahoy_captain .js
+//= link_tree ../../images/ahoy_captain
--- lib/ahoy_captain/engine.rb
+++ lib/ahoy_captain/engine.rb
@@ -12,6 +12,7 @@ module AhoyCaptain
 
     initializer "ahoy_captain.precompile" do |app|
       app.config.assets.paths << AhoyCaptain::Engine.root.join("app/javascript")
+      app.config.assets.paths << AhoyCaptain::Engine.root.join("app/images")
       app.config.assets.precompile << "ahoy_captain/application.js"
     end
   end
