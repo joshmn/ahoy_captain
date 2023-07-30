355f66a892fef47733b07956763b6b868359457c
Josh Brody <git@josh.mn>
2023-07-29 19:10:50 -0500

    fix views per visit

--- app/queries/ahoy_captain/stats/views_per_visit_query.rb
+++ app/queries/ahoy_captain/stats/views_per_visit_query.rb
@@ -2,15 +2,15 @@ module AhoyCaptain
   module Stats
     class ViewsPerVisitQuery < ApplicationQuery
       def build
-
         events = event_query
+                   .joins(:visit)
                    .select("#{::AhoyCaptain.visit.table_name}.started_at as started_at, count(name) / count(distinct visit_id) as views_per_visit")
-                   .where(name: AhoyCaptain.config.view_name)
+                   .where(name: AhoyCaptain.config.event[:view_name])
                    .group("started_at, visit_id")
 
-        visit_query
-          .select("views_per_visit as views_per_visit")
-          .from(events, :views_per_visit_table)
+        ::Ahoy::Visit.ransack(ransack_params).result
+                     .select("views_per_visit as views_per_visit")
+                     .from(events, :views_per_visit_table)
       end
     end
   end
