2d8bd0dbcff3d127d243aa111a51a622b54680aa
Josh Brody <git@josh.mn>
2023-07-30 17:49:27 -0500

    no, actually fix realtime

--- app/controllers/ahoy_captain/realtimes_controller.rb
+++ app/controllers/ahoy_captain/realtimes_controller.rb
@@ -1,7 +1,7 @@
 module AhoyCaptain
   class RealtimesController < ApplicationController
     def show
-      @total = event_query.where('time > ?', 1.minute.ago).distinct(:visit_id).count
+      @total = event_query.where('time > ?', 1.minute.ago).distinct(:visit_id).count(:visit_id)
     end
   end
 end
--- lib/ahoy_captain/version.rb
+++ lib/ahoy_captain/version.rb
@@ -1,3 +1,3 @@
 module AhoyCaptain
-  VERSION = "0.71"
+  VERSION = "0.72"
 end
