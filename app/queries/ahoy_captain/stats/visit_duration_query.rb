32c058bcebe313c5104b30b7c03ed6f7b213c6db
Josh Brody <git@josh.mn>
2023-07-30 09:01:00 -0500

    broken visit druation query

--- app/queries/ahoy_captain/stats/visit_duration_query.rb
+++ app/queries/ahoy_captain/stats/visit_duration_query.rb
@@ -3,13 +3,14 @@ module AhoyCaptain
     class VisitDurationQuery < ApplicationQuery
       def build
         events = event_query
-                   .with_visit
-                   .select("#{::AhoyCaptain.visit_name}.started_at as started_at, visit_id, max(time) - min(time) as visit_duration")
-                   .group("started_at, visit_id")
+          .joins(:visit)
+                   .reselect("ahoy_visits.started_at as started_at, visit_id, max(time) - min(time) as visit_duration")
+                   .group("visit_id")
 
-        visit_query
-                   .reselect("started_at, visit_duration as visit_duration")
-                   .from(events, :visit_durations)
+        ::Ahoy::Visit.joins("INNER JOIN #{::AhoyCaptain.event.table_name} ON #{::AhoyCaptain.event.table_name}.visit_id = visit_durations.visit_id")
+                     .reselect("started_at, avg(visit_duration) as average_visit_duration")
+                     .from(events, :visit_durations)
+
       end
     end
   end
