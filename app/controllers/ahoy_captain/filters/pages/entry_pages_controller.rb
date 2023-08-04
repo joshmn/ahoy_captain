c572a2ca414a6124ad7dd357355a0ba126487993
Josh Brody <git@josh.mn>
2023-08-04 11:34:10 -0500

    fix entry pages and exit page queries

--- app/controllers/ahoy_captain/filters/pages/entry_pages_controller.rb
+++ app/controllers/ahoy_captain/filters/pages/entry_pages_controller.rb
@@ -3,7 +3,8 @@ module AhoyCaptain
     module Pages
       class EntryPagesController < BaseController
         def index
-          query = event_query.all.with_url.distinct_url
+          query = event_query.all.distinct("entry_pages.url").select("entry_pages.url as url")
+
           render json: query.map { |row| { text: row.url } }
         end
 
--- app/controllers/ahoy_captain/filters/pages/exit_pages_controller.rb
+++ app/controllers/ahoy_captain/filters/pages/exit_pages_controller.rb
@@ -3,7 +3,7 @@ module AhoyCaptain
     module Pages
       class ExitPagesController < BaseController
         def index
-          query = event_query.with_url.distinct_url
+          query = event_query.distinct("exit_pages.url").select("exit_pages.url as url")
 
           render json: query.map { |row| { text: row.url } }
         end
