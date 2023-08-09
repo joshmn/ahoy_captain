04f1259d75824626b4191e508bbbe1448d735bba
barseek <sergey.b@hey.com>
2023-08-09 16:16:17 +0200

    query naming

--- app/queries/ahoy_captain/stats/views_per_visit_query.rb
+++ app/queries/ahoy_captain/stats/views_per_visit_query.rb
@@ -1,16 +1,12 @@
 module AhoyCaptain
   module Stats
+    # pls fix
     class ViewsPerVisitQuery < ApplicationQuery
       def build
-        events = event_query
-                   .joins(:visit)
-                   .select("#{::AhoyCaptain.visit.table_name}.started_at as started_at, count(#{AhoyCaptain.event.table_name}.name) / count(distinct #{AhoyCaptain.event.table_name}.visit_id) as views_per_visit")
-                   .where(name: AhoyCaptain.config.event[:view_name])
-                   .group("#{AhoyCaptain.visit.table_name}.started_at, #{AhoyCaptain.event.table_name}.visit_id")
-
-        ::Ahoy::Visit
-                     .select("views_per_visit as views_per_visit")
-                     .from(events, :views_per_visit_table)
+        event_query
+          .joins(:visit)
+          .where(name: "$view")
+          .group("#{AhoyCaptain.visit.table_name}.id")
       end
     end
   end
